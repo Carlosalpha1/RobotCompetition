49f6607d8db7b996cca288e2cd62baea1f12c1e4
Carlos Caminero <carlos2caminero@gmail.com>
2022-08-17 13:48:30 +0200

    update robot_competition_manager with registry

--- robot_competition_manager/src/main.cpp
+++ robot_competition_manager/src/main.cpp
@@ -20,9 +20,55 @@
 #include <robot_competition_msgs/Pose.h>
 #include <robot_competition_msgs/Poses.h>
 
+#include <robot_competition_msgs/registry.h>
+
 #include <iostream>
+#include <vector>
 
 ros::ServiceClient srv_reset_gazebo;
+
+struct player_data
+{
+    std::string name;
+    std::string team;
+    int x_pos;
+    int y_pos;
+};
+
+std::vector<player_data> players_registry;
+
+void show_registry(void)
+{
+    std::cout << "--------------\n";
+    for (auto registry : players_registry) {
+        std::cout << "[" << registry.name << ", " << registry.team << "] "
+                  << "(" << registry.x_pos << ", " << registry.y_pos << ")\n";
+    }
+    std::cout << "--------------\n";
+}
+
+bool register_service_callback(
+    robot_competition_msgs::registry::Request & req,
+    robot_competition_msgs::registry::Response & res)
+{
+    for (auto registry : players_registry) {
+        if (registry.name == req.name && registry.team == req.team) {
+            res.status = false;
+            return true;
+        }
+    }
+    res.status = true;
+    
+    struct player_data entry;
+    entry.name = req.name;
+    entry.team = req.team;
+    entry.x_pos = req.pose.x;
+    entry.y_pos = req.pose.y;
+    players_registry.push_back(entry);
+
+    show_registry();
+    return true;
+}
 
 void ballpose_callback(const robot_competition_msgs::Pose::ConstPtr & msg)
 {
@@ -30,7 +76,7 @@ void ballpose_callback(const robot_competition_msgs::Pose::ConstPtr & msg)
         std_srvs::Empty srv;
         if (srv_reset_gazebo.call(srv)) {
             ROS_INFO("Reseting World");
-        };
+        }
     }
 }
 
@@ -40,9 +86,11 @@ int main(int argc, char **argv)
     ROS_INFO("[robot_competition_manager_node]");
     ros::NodeHandle nh;
     
-    srv_reset_gazebo = nh.serviceClient<std_srvs::Empty>("/gazebo/reset_world");
+    ros::ServiceServer service = nh.advertiseService("register_service", register_service_callback);
 
-    ros::Subscriber ball_pose_sub = nh.subscribe("/ball/pose", 1, ballpose_callback);
+    // srv_reset_gazebo = nh.serviceClient<std_srvs::Empty>("/gazebo/reset_world");
+
+    // ros::Subscriber ball_pose_sub = nh.subscribe("/ball/pose", 1, ballpose_callback);
 
     ros::spin();
     return 0;
